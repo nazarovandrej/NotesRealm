5b243ed0b0f466ecd01030e2211e28a88d063666
Nazarov Andrej <andrejnazarov1987@gmail.com>
2017-07-09 18:23:38 +0300

    Initial commit

---
+++ app/src/main/java/com/github/andrejnazarov/notesrealm/adapter/CustomPagerAdapter.java
@@ -0,0 +1,44 @@
+package com.github.andrejnazarov.notesrealm.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+import com.github.andrejnazarov.notesrealm.BasicFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Nazarov on 09.07.17.
+ */
+
+public class CustomPagerAdapter extends FragmentStatePagerAdapter {
+
+    private List<BasicFragment> mFragmentList;
+
+    public CustomPagerAdapter(FragmentManager fm) {
+        super(fm);
+        mFragmentList = new ArrayList<>();
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return mFragmentList.isEmpty() ? null : mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentList.get(position).getTitle();
+    }
+
+    public void addFragment(BasicFragment fragment) {
+        mFragmentList.add(fragment);
+        notifyDataSetChanged();
+    }
+}
